31d239f4f89886382ff5253c0d8a939cc439f762
elisa-demonki <elisa.demonki@gmail.com>
2016-04-02 00:31:27 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,21 @@
+<?php 
 
+/*
+ * functions.php
+ * Version: 0.1
+ */
+
+// CSS DATEIEN
+// ------------------------------- 
+function hole_die_css_dateien_in_den_header() 
+{ 
+// CSS des Themes
+wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
+// CSS fuer den WP Editor
+add_editor_style( 'editor-style.css' );
+}
+
+add_action('wp_enqueue_scripts', 'hole_die_css_dateien_in_den_header');
+
+
+?>
