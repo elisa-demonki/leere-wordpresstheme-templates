5488faa205e289e704df3ea7076fde78a8d90bad
elisa-demonki <elisa.demonki@gmail.com>
2016-04-02 00:17:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
   <div class="entry">
- 		<?php the_content(); ?>
- 	</div>
+    <?php the_content(); ?>
+  </div>
 
 <?php endwhile; else : ?>
   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
