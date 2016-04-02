150ed8ed8f9ae42af39f10708f10e631449e1097
elisa-demonki <elisa.demonki@gmail.com>
2016-04-02 14:37:10 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,23 +1,35 @@
 <?php 
 
+
 /*
  * functions.php
  * Version: 0.1
  */
 
-// CSS DATEIEN
+
+// CSS das Themes
 // ------------------------------- 
 // Info: https://developer.wordpress.org/reference/functions/wp_enqueue_script/
 
-function hole_die_css_files() 
-{ 
-// CSS des Themes
-wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
-// CSS fuer den WP Editor
-add_editor_style( 'editor-style.css' );
+function hole_die_style_punkt_css() { 
+  
+  wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
+
 }
 
-add_action('wp_enqueue_scripts', 'hole_die_css_files');
+add_action('wp_enqueue_scripts', 'hole_die_style_punkt_css');
 
+
+// CSS fuer den WP Editor
+// ------------------------------- 
+// Info: https://developer.wordpress.org/reference/functions/add_editor_style/
+
+function hole_die_editor_minus_style_punkt_css() {
+  
+  add_editor_style( 'editor-style.css' );
+  
+}
+
+add_action( 'admin_init', 'hole_die_editor_minus_style_punkt_css' );
 
 ?>
