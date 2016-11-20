891c09246a2ffc202102538beeb49a682e68227d
elisa-demonki <elisa.demonki@gmail.com>
2016-11-20 17:04:08 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+
+<!--[if IE 6]>
+<html class="ie ie6" <?php language_attributes(); ?>>
+<![endif]-->
+
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+
+<!--[if !(IE 6) & !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+    
+<head>
 <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
