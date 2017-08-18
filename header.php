0adec9a40cee127289e9f5d828ec03a9542ebe0e
wp-freund <elisa.demonki@gmail.com>
2017-08-18 14:53:27 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
