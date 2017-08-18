fb742460252360ab04e7b3de3747fe0cedee564c
wp-freund <elisa.demonki@gmail.com>
2017-08-18 14:52:07 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-
 <html <?php language_attributes(); ?>>
-    
-<head>
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
